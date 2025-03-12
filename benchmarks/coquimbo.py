a2209fb2a7e3f50eb764b82b056e8e0b242473a4
Jake Moss <jake@outerloop.io>
2025-03-12 10:51:36 +1000

    Add rc benchmarks

--- benchmarks/coquimbo.py
+++ benchmarks/coquimbo.py
@@ -1,14 +1,19 @@
 import os
 from tempfile import TemporaryDirectory
+
+import numpy as np
+
+from aequilibrae.matrix import AequilibraeMatrix
+from aequilibrae.paths import NetworkSkimming, PathResults, RouteChoice as RouteChoiceClass
 from aequilibrae.utils.create_example import create_example
-from aequilibrae.paths import PathResults
-from aequilibrae.paths import NetworkSkimming
 
 
 class GraphBuilding:
     def setup(self):
         self.dir = TemporaryDirectory()
-        self.project = create_example(os.path.join(self.dir.name, "project"), "coquimbo")
+        self.project = create_example(
+            os.path.join(self.dir.name, "project"), "coquimbo"
+        )
 
         self.project.network.build_graphs()
         self.graph = self.project.network.graphs["c"]
@@ -31,7 +36,9 @@ class GraphBuilding:
 class PathComputation:
     def setup(self):
         self.dir = TemporaryDirectory()
-        self.project = create_example(os.path.join(self.dir.name, "project"), "coquimbo")
+        self.project = create_example(
+            os.path.join(self.dir.name, "project"), "coquimbo"
+        )
 
         self.project.network.build_graphs()
         self.graph = self.project.network.graphs["c"]
@@ -60,7 +67,9 @@ class PathComputation:
 class Skimming:
     def setup(self):
         self.dir = TemporaryDirectory()
-        self.project = create_example(os.path.join(self.dir.name, "project"), "coquimbo")
+        self.project = create_example(
+            os.path.join(self.dir.name, "project"), "coquimbo"
+        )
 
         self.project.network.build_graphs()
         self.graph = self.project.network.graphs["c"]
@@ -76,3 +85,73 @@ class Skimming:
     def time_network_skimming(self):
         skm = NetworkSkimming(self.graph)
         skm.execute()
+
+
+class RouteChoice:
+    def setup(self):
+        self.dir = TemporaryDirectory()
+        self.project = create_example(
+            os.path.join(self.dir.name, "project"), "coquimbo"
+        )
+
+        self.project.network.build_graphs()
+        self.graph = self.project.network.graphs["c"]
+        self.graph.set_graph("distance")
+        self.graph.set_blocked_centroid_flows(False)
+
+
+        self.mat = AequilibraeMatrix()
+        self.mat.create_empty(zones=self.graph.num_zones, matrix_names=["demand"], memory_only=True)
+        self.mat.index = self.graph.centroids[:]
+        self.mat.matrices[:, :, 0] = np.full((self.graph.num_zones, self.graph.num_zones), 10.0)
+        self.mat.computational_view()
+
+        self.rc = RouteChoiceClass(self.graph)
+        self.rc.add_demand(self.mat)
+
+    def teardown(self):
+        self.project.close()
+
+    def time_bfsle_with_results(self):
+        self.rc.set_choice_set_generation("bfsle", max_routes=5, seed=12345)
+        self.rc.prepare()
+        self.rc.execute(perform_assignment=False)
+        self.rc.get_results()
+
+    def time_bfsle_without_results(self):
+        self.rc.set_choice_set_generation("bfsle", max_routes=5, seed=12345)
+        self.rc.prepare()
+        self.rc.execute(perform_assignment=False)
+
+    def time_bfsle_assignment_with_results(self):
+        self.rc.set_choice_set_generation("bfsle", max_routes=5, seed=12345)
+        self.rc.prepare()
+        self.rc.execute(perform_assignment=True)
+        self.rc.get_results()
+
+    def time_bfsle_assignment_without_results(self):
+        self.rc.set_choice_set_generation("bfsle", max_routes=5, seed=12345)
+        self.rc.prepare()
+        self.rc.execute(perform_assignment=True)
+
+    def time_lp_with_results(self):
+        self.rc.set_choice_set_generation("lp", max_routes=5, seed=12345)
+        self.rc.prepare()
+        self.rc.execute(perform_assignment=False)
+        self.rc.get_results()
+
+    def time_lp_without_results(self):
+        self.rc.set_choice_set_generation("lp", max_routes=5, seed=12345)
+        self.rc.prepare()
+        self.rc.execute(perform_assignment=False)
+
+    def time_lp_assignment_with_results(self):
+        self.rc.set_choice_set_generation("lp", max_routes=5, seed=12345)
+        self.rc.prepare()
+        self.rc.execute(perform_assignment=True)
+        self.rc.get_results()
+
+    def time_lp_assignment_without_results(self):
+        self.rc.set_choice_set_generation("lp", max_routes=5, seed=12345)
+        self.rc.prepare()
+        self.rc.execute(perform_assignment=True)
